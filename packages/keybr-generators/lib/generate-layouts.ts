ee3236c9517b05b1e69e6379238eb8ef22fd6b57
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2025-06-10 23:04:30 +0530

    feat(keyboard): add Malayalam (India) layout and update character mappings
    Update the Malayalam keyboard layout with additional characters and ligatures

--- packages/keybr-generators/lib/generate-layouts.ts
+++ packages/keybr-generators/lib/generate-layouts.ts
@@ -75,6 +75,7 @@ for (const [id, layout] of [
   ["it_it", importCldr("cldr-keyboards-43.0/keyboards/windows/it-t-k0-windows.xml")],
   ["ja_jp_jis", importKeymap("layouts/ja_jp_jis.json")],
   ["lt_lt", importCldr("cldr-keyboards-43.0/keyboards/windows/lt-t-k0-windows-lt1582.xml")],
+  ["ml_in", importCldr("cldr-keyboards-43.0/keyboards/windows/ml-t-k0-windows.xml")],
   ["nb_kvikk", importKeymap("layouts/nb_kvikk.json")],
   ["nb_no", importCldr("cldr-keyboards-43.0/keyboards/windows/nb-t-k0-windows.xml")],
   ["nb_skarp", importKeymap("layouts/nb_skarp.json")],
